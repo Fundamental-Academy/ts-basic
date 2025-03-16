1777c6265c6194108b90d990fcb0172483bfdb5b
dipadana <dipadanadev@gmail.com>
2025-03-16 12:52:13 +0800

    chore: add several typescript code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+// Basic Type and Variable
+
+let myName: string = "Ketut";
+
+let myAge: number = 25;
+
+let isStudent: boolean = true;
+
+
+
+// Basic Array
+
 const fruits = ["Apple", "Banana", "Cherry"];
 
 fruits.push("Grape");
@@ -8,15 +20,9 @@ const randomThings: (string | number | boolean)[] = ["Tomato", 2, true];
 
 console.log(randomThings)
 
-// ---------------------------
 
-function greet(name?: string): string {
-  return `Hello, ${name ?? "Brother"}`;
-}
 
-console.log(greet("Kadek"))
-
-// ---------------------------
+// Basic Object
 
 type PersonalInfo = {
   name: string;
@@ -44,6 +50,51 @@ myPersonalInfo.name = "Putu"
 myPersonalInfo.age = 30
 
 console.log(myPersonalInfo)
+
+
+
+// Basic Function
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(add(5, 3))
+
+function greet(name?: string): string {
+  return `Hello, ${name ?? "Brother"}`;
+}
+
+console.log(greet("Kadek"))
+
+function stringToWordArray(str: string): string[] {
+  return str.split(" ");
+}
+
+console.log(stringToWordArray("Hello World"))
+
+function wordArrayToString(arr: string[]): string {
+  return arr.join(" ");
+}
+
+console.log(wordArrayToString(["Hello", "World"]))
+
+
+
+// Basic Class
+
+// class Person {
+//   name: string;
+//   age?: number;
+//   constructor(name: string, age?: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   greet(): string {
+//     return `Hello, my name is ${this.name}`;
+//   }
+// }
+
 
 // ---------------------------
 
